d30511cdcb2bbd6b862e22a4e462d01ee23fb0d4
Philip Trauner <philip.trauner@arztpraxis.io>
2018-03-03 20:55:40 +0100

    Removed OpenCV build, fixed random build failure, corrected dependencies

--- package-wallaby-debs.sh
+++ package-wallaby-debs.sh
@@ -6,7 +6,7 @@ export CC=arm-linux-gnueabihf-gcc
 export CXX=arm-linux-gnueabihf-g++
 export CFLAGS="-I/usr/include -I/usr/local/include -march=armv7"
 export CXXFLAGS="$CFLAGS"
-export OpenCV_DIR="`pwd`/wallaby-build/opencv-2.4.12.3/build"
+export OpenCV_DIR="/usr/include/opencv2/"
 
 function build_package {
 	local name=(${1//// })
@@ -22,6 +22,9 @@ function build_package {
 	cmake $4 ..
 	make -j`nproc`
 
+	# checkinstall sucks
+	sudo make install
+
 	echo "$name for Wallaby" > description-pak
 	sudo checkinstall \
 	        --default \
@@ -30,28 +33,10 @@ function build_package {
 	        --maintainer="me@christoph-heiss.me" \
 	        --pkggroup=wallaby \
 		--requires=$3 \
-		--backup=no
+		--backup=no \
+		--fstrans=yes
 
 	cp "${name}_`date +%Y%m%d`-1_armhf.deb" ../../debs/
-	cd ../..
-}
-
-function build_opencv {
-	wget "https://github.com/opencv/opencv/archive/2.4.12.3.tar.gz"
-	tar -xf "2.4.12.3.tar.gz"
-
-	cd opencv-2.4.12.3
-	mkdir build
-	cd build
-
-	cmake \
-		-DBUILD_DOCS=OFF -DBUILD_PACKAGE=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF \
-		-DBUILD_opencv_apps=OFF -DBUILD_opencv_calib3d=OFF -DBUILD_opencv_gpu=OFF \
-		-DBUILD_opencv_flann=OFF -DBUILD_opencv_legacy=OFF -DBUILD_opencv_ml=OFF \
-		-DBUILD_opencv_nonfree=OFF -DBUILD_opencv_ocl=OFF -DBUILD_opencv_superres=OFF \
-		-DBUILD_opencv_video=OFF -DBUILD_opencv_videostab=OFF ..
-	make -j`nproc`
-
 	cd ../..
 }
 
@@ -59,24 +44,18 @@ function build_opencv {
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-
 rm -rf wallaby-build
 mkdir -p wallaby-build/debs
 
 cd wallaby-build
-
-build_opencv
 
 build_package kipr/bsonbind master
 build_package kipr/libkar use_Qt4 "libqt4-dev"
 build_package kipr/pcompiler use_Qt4 "libqt4-dev,libkar"
 build_package kipr/daylite master "libbson-dev,libboost-dev,bsonbind"
 build_package kipr/libaurora master "libpng-dev,zlib1g-dev"
-build_package kipr/libwallaby master "libzbar-dev,libopencv-dev,libjpeg-dev,python-dev" "-DBUILD_DOCUMENTATION=OFF -Dbuild_python=OFF"
+build_package F-WuTS/libwallaby master "libcv-dev,libjpeg-dev" "-DBUILD_DOCUMENTATION=OFF -Dbuild_python=OFF"
 build_package kipr/libbotball master "libaurora,libwallaby"
-build_package robot0nfire/botui master "libqt4-dev,pcompiler,libssl-dev"
-
-cd debs/
-wget "http://ftp.debian.org/debian/pool/main/libb/libbson/libbson-1.0-0_1.6.3-1_armhf.deb"
+build_package F-WuTS/botui master "libcv-dev,libqt4-dev,pcompiler"
 
 sudo dpkg -r botui libbotball libwallaby libaurora daylite pcompiler libkar bsonbind
